0ac60bb23e9121df294f218e48be8d9f992ae030
AndriantoAGun <andrianto22491@gmail.com>
2019-07-18 07:31:48 +0700

    Tugas 2 JavaScript
    tugas Niomic tentang JavaScript meliputi jenis-jenis Type data

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+const number = 39;
+const string = "Heyo";
+const truth = true;
+const empty = null;
+const array = [93, "Oira", false, undefined];
+
+console.log(number);
+console.log(string);
+console.log(truth);
+console.log(empty);
+console.log(array[1]);
